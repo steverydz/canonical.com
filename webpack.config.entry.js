c06f5d9382fd43f1b03ab8fd82ca3e3ed98de2b3
Steve Rydz <steve.rydz@canonical.com>
2020-07-27 15:43:36 +0100

    Move careers game to JS build pipeline

--- webpack.config.entry.js
+++ webpack.config.entry.js
@@ -2,5 +2,6 @@ module.exports = {
   "careers-filter-and-sort": ["./static/js/src/careers-filter-and-sort.js"],
   "apply-for-jobs": ["./static/js/src/apply-for-jobs.js"],
   "file-validation": ["./static/js/src/file-validation.js"],
+  "careers-game": ["./static/js/src/careers-game.js"],
   tabs: ["./static/js/src/tabs.js"],
 };
