ca0ea4e2356655c7fbd4b31bbf4b840233493aab
Steve Rydz <steve.rydz@canonical.com>
2020-07-27 15:41:24 +0100

    Move apply-for-jobs to JS build pipeline

--- webpack.config.entry.js
+++ webpack.config.entry.js
@@ -1,5 +1,6 @@
 module.exports = {
   "careers-filter-and-sort": ["./static/js/src/careers-filter-and-sort.js"],
+  "apply-for-jobs": ["./static/js/src/apply-for-jobs.js"],
   "file-validation": ["./static/js/src/file-validation.js"],
   tabs: ["./static/js/src/tabs.js"],
 };
